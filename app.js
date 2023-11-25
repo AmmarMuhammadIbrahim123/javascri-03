02be4b66c2857b3cff043342f31fd2629c86df26
AmmarMuhammadIbrahim123 <138091591+AmmarMuhammadIbrahim123@users.noreply.github.com>
2023-11-25 07:30:28 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,183 @@
+// QUESTION # 01
+// var arr=[];
+// document.write(arr);
+
+
+// QUESTION # 02
+// var arr=[];
+// document.write(arr);
+
+
+// QUESTION # 03
+// var arr=["AMMAR"];
+// document.write(arr);
+
+
+// QUESTION # 04
+// var arr=[10];
+// document.write(arr);
+
+// QUESTION # 05
+// var arr=[true,false];
+// document.write(arr);
+
+
+// QUESTION # 06
+// var arr=["AMMAR",  10,  3.14,  true];
+// document.write(arr);
+
+// QUESTION # 07
+//  const educationQualifications = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil.', 'PhD'];
+
+// document.write('<h2> Qualifications :</h2>');
+// document.write('<ol>');
+
+// for (let i = 0; i < educationQualifications.length; i++) {
+//     document.write('<li>' + educationQualifications[i] + '</li>');
+// }
+
+// document.write('</0l>');
+
+// QUESTION # 08
+
+// const studentName=["ammar"];
+// const score = [320];
+// const total = [500];
+// const result =   score / total *100;
+// console.log(`Score of Ammar is.Percentage:  ${result}%` );
+
+
+// const studentName2=["saad"];
+// const score2 = [480];
+// const total2 = [500]
+// const result2 =   score2 / total2 *100;
+// console.log(`Score of SAAD is.Percentage :  ${result2}%` );
+
+
+// const studentName3=["uamir"];
+// const score3 = [430];
+// const total3 = [500]
+// const result3 =   score3 / total3 *100;
+// console.log(`Score of UMAIR is.Percentage:  ${result3}%` );
+
+
+// QUESTION # 09
+
+//  var arr=[ "RED","GREEN", "YELLOW"];
+//  document.write("BEFORE UPDATING:"+ arr);
+// // (a);
+ 
+//  document.write(arr);
+  
+// // (b);
+//  var userinput= prompt( "add what color you want to add");
+
+// arr.unshift("AFTER UPDATION:" +userinput);
+// document.write(arr);
+
+// // (c);
+
+//  var userinput= prompt( "add what color you want to add");
+
+// arr.push("AFTER UPDATION:" +userinput);
+// document.write(arr);
+
+// // (d)
+//  var userinput= prompt( "add what color you want to add");
+
+// arr.shift("AFTER UPDATION:" +userinput);
+// document.write(arr);
+
+// // (e)
+//  var userinput= prompt( "add what color you want to add");
+//  arr.pop("AFTER UPDATION:" +userinput);
+//   document.write( arr);
+
+
+// // (F)
+// var index= prompt("ENTER INDEX VALUE FOR ADD COLOR");
+// var colorName=prompt("ENTER COLOR NAME YOU WANT TO ADD");
+// arr.splice(index, 0, colorName);
+// document.write(" <br> <br>AFTER UPDATING:" +arr);
+
+
+// // (g)
+// var indexStart= prompt("ENTER  START INDEX VALUE FOR REMOVE COLOR");
+// var indexEnd=prompt("ENTER LAST INDEX  VALUE YOU WANT TO REMOVE");
+// arr.splice(indexStart, indexEnd,);
+// document.write( "<br> <br> AFTER UPDATIG:" +arr);
+
+// // QUESTION # 10
+/////////////// CONFUSION /////////////////////////////
+
+
+
+
+// // QUESTION # 11
+//  var cities=[    "Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"];
+//  document.write( "CITIES:" +cities);
+//  var selectedCities=[];
+
+//  selectedCities=cities.splice(1,3);
+//  document.write( "<br> <br> SELECTED CITIES:"+ selectedCities);
+
+
+// // QUESTION # 12
+// var arr = ["This", " is", " my", " cat"];
+// var resultString = arr.join("");
+// document.write(resultString);
+
+
+// // // QUESTION # 13
+// // Create a new empty array to simulate a queue
+// var fifoQueue = [];
+
+// // Add values to the queue using push (enqueue)
+// fifoQueue.push("Value 1");
+// fifoQueue.push("Value 2");
+// fifoQueue.push("Value 3");
+
+// // Display the current state of the queue
+// document.write("  Queue after enqueue:", fifoQueue);
+  
+// // // QUESTION # 14 
+
+// // Remove values from the queue using shift (dequeue)
+// var removedValue = fifoQueue.shift();
+// document.write("  <br> <br> Dequeued value:", removedValue);
+
+// // Display the updated state of the queue
+// document.write(" <br> <br> Queue after dequeue:", fifoQueue);
+
+
+ // // QUESTION # 15 // //
+ //////////// CONFUSION ///////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
